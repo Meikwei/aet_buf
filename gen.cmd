978e570faee5940329803958d42bfafef936eea5
zhangkaiwei <1126763237@qq.com>
2024-05-23 21:08:35 +0800

    aet_buf init

---
+++ gen.cmd
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+rem Define array elements
+set "PROTO_NAMES=uint32 sys_user"
+
+rem Loop through each element in the array
+for %%i in (%PROTO_NAMES%) do (
+    rem Generate Go code for Protobuf
+    protoc --go_out=./%%i --go_opt=module=github.com/Meikwei/aet_buf/%%i %%i/%%i.proto
+    rem Generate Go gRPC code for Protobuf
+    protoc --go-grpc_out=./%%i --go-grpc_opt=module=github.com/Meikwei/aet_buf/%%i %%i/%%i.proto
+    protoc --validate_out=lang=go,paths=source_relative:./ %%i/%%i.proto
+    protoc --grpc-gateway_out=./%%i --grpc-gateway_opt=module=github.com/Meikwei/aet_buf/%%i %%i/%%i.proto
+    protoc --openapiv2_out=./ %%i/%%i.proto
+    if ERRORLEVEL 1 (
+        echo error processing %%i.proto
+        exit /b %ERRORLEVEL%
+    )
+)
+
+rem Replace "omitempty" in *.pb.go files with UTF-8 encoding
+for /r %%f in (*.pb.go) do (
+    powershell -Command "(Get-Content -Path '%%f' -Encoding UTF8) -replace ',omitempty\"`"', '\"`"' | Set-Content -Path '%%f' -Encoding UTF8"
+)
+
+
+endlocal
---
+++ tool/tool.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: zhangkaiwei 1126763237@qq.com
+ * @Date: 2024-05-19 08:14:08
+ * @LastEditors: zhangkaiwei 1126763237@qq.com
+ * @LastEditTime: 2024-05-19 08:39:03
+ * @FilePath: \aet_buf\tool\tool.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package tool
+
+import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
